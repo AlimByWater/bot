ec474a06767291eed1fe58526856b10306eee86f
a.tsagaraev <a.tsagaraev@mtlab.space>
2024-07-01 09:46:23 +0300

    migrations

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,11 @@
+-- Удаление таблицы воспроизведения песен
+DROP TABLE IF EXISTS song_plays;
+
+-- Удаление таблицы загрузок песен пользователями
+DROP TABLE IF EXISTS user_song_downloads;
+
+-- Удаление таблицы песен
+DROP TABLE IF EXISTS songs;
+
+-- Удаление таблицы пользователей
+DROP TABLE IF EXISTS users;
