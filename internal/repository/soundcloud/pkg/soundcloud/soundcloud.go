cb822d6dcea9e4d917e4353ac2a034bfc935bccb
a.tsagaraev <a.tsagaraev@mtlab.space>
2024-08-28 13:33:43 +0300

    useragent changed

--- internal/repository/soundcloud/pkg/soundcloud/soundcloud.go
+++ internal/repository/soundcloud/pkg/soundcloud/soundcloud.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	userAgent           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+	userAgent           = "PostmanRuntime/7.41.2"
 	version             = "2.3.8"
 	defaultDownloadPath = "./"
 )
